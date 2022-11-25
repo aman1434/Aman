6c0428b71a2544c7c1fd32abdf42d80ab46a5416
aman14345 <kumar14345aman@gmail.com>
2022-11-26 01:05:05 +0530

    arrays

--- Arrays and array linked list/src/aman/AreaylistExample.java
+++ Arrays and array linked list/src/aman/AreaylistExample.java
@@ -1,7 +1,13 @@
 package aman;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
 public class AreaylistExample {
     public static void main(String[] args) {
-
+        ArrayList<Integer> list=new ArrayList(10);
+        list.add(67);
+        list.add(876);
+        System.out.println(list);
     }
 }
